a864e750b3ec0cc9862d731a398a3adcee8fdf82
devin-lynch <devin.l.lynch@gmail.com>
2022-09-29 13:32:28 -0500

    Add Timer.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Wisdom from './Wisdom';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
       <Wisdom />
+      <Timer />
     </div>
   );
 }
